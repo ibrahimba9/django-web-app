e910e5881cab32ff7f41a3b445643eaf0d1f5d0f
unknown <ibrahim.benabdallah@esprit.tn>
2023-09-08 20:27:43 +0200

    [develop] add templates and render data from views

--- merchex/listings/views.py
+++ merchex/listings/views.py
@@ -6,39 +6,18 @@ from .models import Band, Listing
 
 def hello(request):
     bands = Band.objects.all()
-    list_elements: str = ""
-    for band in bands:
-        list_elements += "<li>{}</li>".format(band.name)
-
-    return HttpResponse(
-        f"""
-        <h1>Hello Merchex</h1>
-        <p> My favorite bands are the following:<p>
-        <ul>{list_elements}<ul>
-    """
-    )
+    return render(request, "listings/hello.html", {"bands": bands})
 
 
 def about(request):
-    return HttpResponse("<h1>À propos</h1> <p>Nous adorons merch !</p>")
+    return render(request, "listings/about.html")
 
 
 def contact(request):
-    return HttpResponse(
-        "<h1>Contact us</h1> <p>Pour nous contacter n'hésitez pas à nous joindre sur l'adresse suivante : <strong>contact@merchex.com</strong></p>"
-    )
+    return render(request, "listings/contact.html")
 
 
 def listings(request):
     all_listings = Listing.objects.all()
     list_elements: str = ""
-    for listing in all_listings:
-        list_elements += "<li>{}</li>".format(listing.title)
-    return HttpResponse(
-        f"""
-        <h1>Liste des annonces</h1>
-        <ul>
-            {list_elements}
-        </ul>
-    """
-    )
+    return render(request, "listings/listings.html", {"listings": all_listings})
