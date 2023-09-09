9db006198d7dada2a5d5bc6dc48d1d1d92a133f3
unknown <ibrahim.benabdallah@esprit.tn>
2023-09-09 15:48:32 +0200

    [develop] register models in django admin interface

--- merchex/listings/admin.py
+++ merchex/listings/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
-# Register your models here.
+from listings.models import Band, Listing
+
+
+class BandAdmin(admin.ModelAdmin):
+    list_display = ("name", "year_formed", "genre")
+
+
+class ListingAdmin(admin.ModelAdmin):
+    list_display = ("title", "year", "description")
+
+
+admin.site.register(Band, BandAdmin)
+admin.site.register(Listing, ListingAdmin)
--- merchex/listings/models.py
+++ merchex/listings/models.py
@@ -1,9 +1,48 @@
+from django.core.validators import (
+    MinValueValidator,
+    MaxValueValidator,
+    MaxLengthValidator,
+)
 from django.db import models
 
 
 class Band(models.Model):
+    class Genre(models.TextChoices):
+        HIP_HOP = "HH"
+        SYNTH_POP = "SP"
+        ALTERNATIVE_ROCK = "AR"
+        METAL = "M"
+        RHYTHM_N_BLUES = "RNB"
+
     name = models.fields.CharField(max_length=100)
+    genre = models.fields.CharField(choices=Genre.choices, max_length=5)
+    biography = models.fields.CharField(max_length=1000)
+    year_formed = models.fields.IntegerField(
+        validators=[MinValueValidator(1900), MaxValueValidator(2023)]
+    )
+    active = models.fields.BooleanField(default=True)
+    official_homepage = models.fields.URLField(null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.name}"
 
 
 class Listing(models.Model):
+    class ListingType(models.Choices):
+        RECORDS = "records"
+        CLOTHING = "clothing"
+        POSTERS = "posters"
+        Miscellaneous = "miscellaneous"
+
     title = models.fields.CharField(max_length=100)
+    description = models.fields.CharField(max_length=1000)
+    sold = models.fields.BooleanField(default=False)
+    year = models.fields.IntegerField(
+        blank=True,
+        null=True,
+        validators=[MinValueValidator(1900), MaxLengthValidator(2023)],
+    )
+    type = models.fields.CharField(choices=ListingType.choices, max_length=20)
+
+    def __str__(self):
+        return f"{self.title}"
