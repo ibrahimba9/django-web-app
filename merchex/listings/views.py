20b045cbe36b8820d62e4718a4b44c6b14af73df
unknown <ibrahim.benabdallah@esprit.tn>
2023-09-12 21:00:37 +0200

    [develop] add list and detail pages to each model and link them

--- merchex/listings/views.py
+++ merchex/listings/views.py
@@ -1,12 +1,24 @@
-from django.http import HttpResponse
+from django.core.exceptions import ObjectDoesNotExist
+from django.http import Http404, HttpResponse
 from django.shortcuts import render
 
 from .models import Band, Listing
 
 
-def hello(request):
+def band_list(request):
     bands = Band.objects.all()
-    return render(request, "listings/hello.html", {"bands": bands})
+    return render(request, "listings/band_list.html", {"bands": bands})
+
+
+def band_detail(request, band_id):
+    try:
+        band = Band.objects.get(id=band_id)
+        return render(request, "listings/band_detail.html", {"band": band})
+    except Band.DoesNotExist:
+        # raise Http404("No Band matches the given query.")
+        return HttpResponse(
+            f"Oops! No Band with the id {band_id} was found.", status=404
+        )
 
 
 def about(request):
@@ -17,7 +29,20 @@ def contact(request):
     return render(request, "listings/contact.html")
 
 
-def listings(request):
+def listing_list(request):
     all_listings = Listing.objects.all()
-    list_elements: str = ""
-    return render(request, "listings/listings.html", {"listings": all_listings})
+    return render(request, "listings/listing_list.html", {"listings": all_listings})
+
+
+def listing_detail(request, listing_id):
+    try:
+        listing = Listing.objects.get(id=listing_id)
+        return render(request, "listings/listing_detail.html", {"listing": listing})
+    except ObjectDoesNotExist as exc:
+        return HttpResponse(
+            f"Oops! No Listing with the id {listing_id} was found.", status=404
+        )
+
+
+def page_not_found(request):
+    raise ObjectDoesNotExist
