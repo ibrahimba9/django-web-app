0efb25c962bc159d5827b4025742c8daaafb013c
unknown <ibrahim.benabdallah@esprit.tn>
2023-09-12 21:38:56 +0200

    [develop] add form, validate data and redirect on sucess

--- merchex/listings/views.py
+++ merchex/listings/views.py
@@ -1,7 +1,9 @@
 from django.core.exceptions import ObjectDoesNotExist
+from django.core.mail import send_mail
 from django.http import Http404, HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
+from .forms import ContactUsForm
 from .models import Band, Listing
 
 
@@ -26,7 +28,23 @@ def about(request):
 
 
 def contact(request):
-    return render(request, "listings/contact.html")
+    if request.method == "POST":
+        form = ContactUsForm(request.POST)
+        if form.is_valid():
+            send_mail(
+                subject=f'Message from {form.cleaned_data["name"] or "anonyme"} via MerchEx Contact Us form',
+                message=form.cleaned_data["message"],
+                from_email=form.cleaned_data["email"],
+                recipient_list=["admin@merchex.xyz"],
+            )
+            return redirect("email-sent")
+    else:
+        form = ContactUsForm()
+    return render(request, "listings/contact.html", {"form": form})
+
+
+def email_sent(request):
+    return render(request, "listings/email_sent.html")
 
 
 def listing_list(request):
